3c6e60daf11df640c95f39a0e9fe068a109ac2bf
Haifeng Wang <haifeng@udel.edu>
2017-11-07 13:39:24 -0500

    flask script added

--- songbase.py
+++ songbase.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>hello world!!!!!!</h1>'
+    return '<h1>hello world!!!!!! lalallal</h1>'
 
 @app.route('/user/<string:name>/')
 def get_user(name):
     return '<h1>hello %s your age is %d</h1>' % (name, 20)
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
